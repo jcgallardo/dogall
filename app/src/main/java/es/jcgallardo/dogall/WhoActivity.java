7f24ae61b7c0b29ec8839ec92806fbf3bc71c38c
jcgallardo <jcgallardomorales@gmail.com>
2017-05-02 23:50:35 +0200

    Segundo commit

--- app/src/main/java/es/jcgallardo/dogall/WhoActivity.java
+++ app/src/main/java/es/jcgallardo/dogall/WhoActivity.java
@@ -1,14 +1,28 @@
 package es.jcgallardo.dogall;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WhoActivity extends Activity {
+    Button b_seguir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_who);
+
+        b_seguir = (Button) findViewById(R.id.b_seguir);
+
+        b_seguir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent siguiente = new Intent(WhoActivity.this,ColeccionActivity.class);
+                startActivity(siguiente);
+            }
+        });
     }
 }
